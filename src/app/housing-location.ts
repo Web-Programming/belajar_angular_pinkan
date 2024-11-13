9cc4ef34a88bfc185aae89cbaf44c9a8eefdff9e
pinkanpriharini <126873421+pinkanpriharini@users.noreply.github.com>
2024-11-13 20:29:32 +0700

    angular housing

---
+++ src/app/housing-location.ts
@@ -0,0 +1,10 @@
+export interface HousingLocation {
+    id: number,
+    name: String,
+    city: String,
+    state: string,
+    photo: string,
+    available: number,
+    wifi: boolean,
+    laundry: boolean,
+}
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -3,14 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
-    <p>
-      housing-location works!
-    </p>
+    <section>
+    <ing class="Listing-Photo" [src]="HousingLocation.photo" alt="Exterior photo of {{ housingLocation.name}}">
+    <h2 class="listing-heading">{{
   `,
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation: HousingLocation
 }

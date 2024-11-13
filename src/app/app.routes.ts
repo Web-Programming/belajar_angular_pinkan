92ff3ed1beeb4442f645703313be85cefac4fd14
pinkanpriharini <126873421+pinkanpriharini@users.noreply.github.com>
2024-11-13 19:53:55 +0700

    pinkan

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,11 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
 import { HomeComponent } from './home/home.component';
+export const router: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent,
+        title: 'Home Page'
+    }];
+
 
 export const routes: Routes = [{path: 'home', component: HomeComponent},];
